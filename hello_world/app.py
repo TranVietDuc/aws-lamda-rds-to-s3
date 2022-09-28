189de8d95781aa0d80c4aac57185fd1afa6d6897
DUCS-THINKPAD\Think pard <vietduc3k@gmail.com>
2022-09-28 10:29:28 +0700

    push all tble

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,8 @@
 from io import StringIO
-import os
 import pymysql
 import json
 import boto3
-import datetime
-import csv
+from datetime import datetime, timedelta
 import pandas as pd
 
 # import requests
@@ -18,37 +16,49 @@ db = "ao_dai_gia_bach"
 connection = pymysql.connect(host=host, user=username, passwd=password,db=db)
 s3 = boto3.client('s3')
 
-#def lambda_handler(event,context):
-def lambda_handler():
+def getSQLSatementsByTable(tableName):
+    if tableName == "category" or tableName =="customer" or tableName == "product":
+        return "SELECT * FROM {}".format(tableName)
+    elif tableName == "orders" or tableName == "orderdetail":
+        return """SELECT * FROM {} 
+        WHERE UpdatedAt BETWEEN CURDATE() - INTERVAL 1 DAY
+        AND CURDATE() - INTERVAL 1 SECOND""".format(tableName)
+    else: raise Exception("Table is not existing!!!")    
+
+def getFileFolderByTable(tableName):
+    lastDayString = (datetime.now() - timedelta(1)).strftime("%Y/%m/%d/")
+    return "raw/{}/{}".format(tableName,lastDayString)
+
+def lambda_handler(event,context):
     try:
-        tableName = "customer"
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM {}'.format(tableName))
-        rows = cursor.fetchall()
+        listTableName = ["category", "customer", "product", "orders", "orderdetail"]
         
-        bucket = "ao-dai-gia-bach"
-        todayString = datetime.datetime.now().strftime("%Y/%m/%d/")
-        csvFileName = tableName + ".csv"
-        fileObject = todayString + csvFileName
-
-        #handle csvfile local
-
-        #csvFile = open(csvFileName, mode='w+', newline='',encoding='utf-8')
-        #fileWriter = csv.writer(csvFile)
-        #fileWriter.writerows(rows)
-        #csvFile.close()
-
-        #load data to dataFrame
-        df = pd.DataFrame.from_records(rows)
-        csv_buffer = StringIO()
-        df.to_csv(csv_buffer, index=False, header=None)
-        s3.put_object(Body=csv_buffer.getvalue(),Bucket=bucket,Key=fileObject)
-        print(csv_buffer.getvalue)
+        # Loop for tables
+        for tableName in listTableName:
+            #Get data
+            cursor = connection.cursor()
+            sqlStatement = getSQLSatementsByTable(tableName=tableName)
+            cursor.execute(sqlStatement)
+            rows = cursor.fetchall()
 
 
-        # with open(csvFileName, mode='r', encoding='utf-8') as file:
-        #     s3.upload_file(csvFileName,bucket, fileObject)
+            #Get columns' name as header
+            getHeaderStatement = """SELECT group_concat(COLUMN_NAME) 
+            FROM INFORMATION_SCHEMA.COLUMNS 
+            WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = '{}';""".format(db, tableName)
+            cursor.execute(getHeaderStatement)
+            header = cursor.fetchone()[0].split(",")
 
+            bucket = "ao-dai-gia-bach"
+            csvFileName = tableName + ".csv"
+            fileObject = getFileFolderByTable(tableName=tableName) + csvFileName
+            
+            #load data to S3
+            if(rows):
+                df = pd.DataFrame.from_records(rows)
+                csv_buffer = StringIO()
+                df.to_csv(csv_buffer, index=False, header=header)
+                s3.put_object(Body=csv_buffer.getvalue(),Bucket=bucket,Key=fileObject)
 
         print('put seccessfully!')
     except Exception as exception:
@@ -62,7 +72,6 @@ def lambda_handler():
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "message": str(df)
+            "message": "put seccessfully!"
         })
     }
-lambda_handler()
